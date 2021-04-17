e0af5b56a411af6d8f596a2ffafd4eb11ad6e4ed
ntewolde <nahom.tewolde@outlook.com>
2021-04-17 02:27:39 -0500

    Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+	public class StandardGradeBook : BaseGradeBook
+	{
+		public StandardGradeBook(string name) : base(name)
+		{
+			Type = Enums.GradeBookType.Standard;
+		}
+		
+		
+		
+	}
+}
